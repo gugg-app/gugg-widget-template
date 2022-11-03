7e2eb00a18713c7505846166f07c09d341ea8a89
Johannes Fritsch <fritsch@zoom7.de>
2022-11-03 16:58:44 +0100

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import * as React from "preact";
+import { render } from "preact";
+import './index.css';
+
+const body = document.getElementById("root");
+render(<div className="m-10">Hello World</div>, body);
