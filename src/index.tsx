7c5a82142c37903460abede619d051cc47433abb
Johannes Fritsch <fritsch@zoom7.de>
2023-01-19 23:16:01 +0100

    Basic version

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,4 @@ import { render } from "preact";
 import './index.css';
 
 const body = document.getElementById("root");
-render(<div className="m-10">Hello World</div>, body);
+render(<div className="bg-black text-white overflow-hidden select-none" style="height: 100vh;">Hello World! Give me a high-five!</div>, body);
